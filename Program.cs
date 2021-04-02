7800363588d35d2a00ff9175c14743e239835003
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-04-02 11:23:28 +0300

    Average done

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
             var name = Console.ReadLine();
             Console.WriteLine($"Hello {name!}");
 
-            MultTable();
+            Average();
         }
 
         static int Sum(){
@@ -83,6 +83,19 @@ namespace HelloWorld
                     Console.WriteLine($"{num} X {i} = {num * i }");
                 
                 }
+        }
+        static void Average(){
+                Console.WriteLine("First number?");
+                var first = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Second number?");
+                var second = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Third number?");
+                var third = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Fourth number?");
+                var fourth = Convert.ToDouble(Console.ReadLine());
+                
+                var result = (first + second + third + fourth)/4;
+                Console.WriteLine($"The average of {first} , {second} , {third} , {fourth} is: {result}");
         }
     
     }
