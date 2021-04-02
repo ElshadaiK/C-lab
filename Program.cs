d2ccd0b9d632d343049afc2f88c5b7b7c560c792
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-04-02 11:19:44 +0300

    7 questions done

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name!}");
+
+            MultTable();
+        }
+
+        static int Sum(){
+            Console.WriteLine("Enter a");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            var res = a+b;
+            Console.WriteLine(res);
+            return res;
+        }
+
+        static double Div(){
+            Console.WriteLine("Enter a");
+            var a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b");
+            var b = Convert.ToDouble(Console.ReadLine());
+
+            double res = a/b;
+            Console.WriteLine(res);
+            return res;
+        }
+        static double Calc(string expression){
+            var res = expression.Split();
+            Console.WriteLine(res);
+
+            return 0;
+        }
+    
+        static void Swapper(){
+            Console.WriteLine("First number?");
+            var first = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Second number?");
+            var second = Convert.ToDouble(Console.ReadLine());
+
+            var mult = first * second;
+            first = mult / first;
+            second = mult / first;
+            Console.WriteLine($"First number: {first} Second number: {second}");
+        }
+
+        static void Multiplier(){
+                Console.WriteLine("First number?");
+                var first = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Second number?");
+                var second = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Third number?");
+                var third = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"{first} X {second} X {third} = {first * second * third}");
+            }
+        
+        static void AllOperations(){
+                Console.WriteLine("First number?");
+                var first = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Second number?");
+                var second = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"{first} + {second} = {first + second }");
+                Console.WriteLine($"{first} - {second} = {first - second }");
+                Console.WriteLine($"{first} X {second} = {first * second }");
+                Console.WriteLine($"{first} / {second} = {first / second }");
+                Console.WriteLine($"{first} mod {second} = {first % second }");
+            }
+        static void MultTable(){
+                Console.WriteLine("Enter number?");
+                var num = Convert.ToDouble(Console.ReadLine());
+                for(int i = 0; i<11; i++){
+                    Console.WriteLine($"{num} X {i} = {num * i }");
+                
+                }
+        }
+    
+    }
+}
