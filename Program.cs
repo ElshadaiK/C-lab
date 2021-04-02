b46d704c7c4f58eb7d095618c55f6cd127f7e65f
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-04-02 11:27:22 +0300

    Associative finished

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
             var name = Console.ReadLine();
             Console.WriteLine($"Hello {name!}");
 
-            Average();
+            Associative();
         }
 
         static int Sum(){
@@ -97,6 +97,17 @@ namespace HelloWorld
                 var result = (first + second + third + fourth)/4;
                 Console.WriteLine($"The average of {first} , {second} , {third} , {fourth} is: {result}");
         }
+        static void Associative(){
+                Console.WriteLine("First number?");
+                var first = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Second number?");
+                var second = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Third number?");
+                var third = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Result of specified numbers {first}, {second}, and {third}, (x+y).z is {(first+second)*third} and x.y + y.z is {(first*second) + (second*third)}");
+            }
+        
     
     }
 }
