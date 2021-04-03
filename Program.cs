70e9bde9cb19a2f30bb314d65922d0f6eaa598dd
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-04-03 11:06:12 +0300

    Check 20 added

--- Program.cs
+++ Program.cs
@@ -243,6 +243,23 @@ namespace HelloWorld
                 
             }
         
+        static bool Check20(){
+                Console.WriteLine("First number?");
+                var first = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Second number?");
+                var second = Convert.ToInt32(Console.ReadLine());
+                var result = false;
+
+                if((first != second && (first == 20 || second == 20)) || first + second == 20){
+                    result = true;
+                }
+                
+                Console.WriteLine($"Result: {result}");
+                return result;
+
+                
+            }
+        
         
     
     }
