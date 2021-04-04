a543f2cbcd9b49a04dbf7b2dfd4caebf4672f52f
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-04-04 11:33:47 +0300

    Multiple of 3 and 7 finished

--- Program.cs
+++ Program.cs
@@ -377,5 +377,16 @@ namespace HelloWorld
             return res;
             
         }
+
+        static bool MultipleOf3Or7(){
+            var res = false;
+            Console.WriteLine("Enter the number?");
+            var input = Convert.ToInt32(Console.ReadLine());
+            if(input%7 == 0 || input%3 == 0){
+                res = true;
+            }
+            return res;
+            
+        }
     }
 }
