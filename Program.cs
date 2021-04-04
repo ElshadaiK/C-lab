c047701312135dc15934e79f9a6da605bfc2fc1d
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-04-04 11:42:28 +0300

    First & second number comparison added

--- Program.cs
+++ Program.cs
@@ -399,5 +399,18 @@ namespace HelloWorld
             return res;
             
         }
+
+        static bool FirstSecondComparison(){
+            var res = false;
+            Console.WriteLine("Enter the first number? <100");
+            var first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number? >200");
+            var second = Convert.ToInt32(Console.ReadLine());
+            if(first < 100 && second > 200){
+                res = true;
+            }
+            return res;
+            
+        }
     }
 }
